5e40f04a6b42c9e8fe756baa4847b88583ba7aaa
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-03-08 21:22:58 +0630

    use api

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 import {pinPos} from 'pitaka/align'
+import {stripLinesNote} from 'pitaka/utils'
 import { linePN } from 'pitaka/offtext';
 export const stripNotes=(lines,ctx)=>{
     const notes=ctx.notes;
@@ -6,22 +7,8 @@ export const stripNotes=(lines,ctx)=>{
     notes.forEach( (note,idx)=>{
         NoteIdx[idx+1]=note; //nid is one base, see parser.js::onClose note()
     });
-    lines=lines.map((line,y)=>{
-        let accwidth=0;
-        let nline=line.replace(/⚓(\d+)/g,(m,m1,offset)=>{
-            const note=NoteIdx[m1];
-            if (note) {
-                note[0]=y;
-                note[1]=offset-accwidth;    
-            } else {
-                /* skip note in the first line , difficult to pin */
-                if (y) console.log('note not found',m1,y,line)
-            }
-            accwidth+=m.length;
-            return '';
-        })
-        return nline;
-    })
+    lines=stripLinesNote(lines,NoteIdx);
+
     notes.forEach(note=>{
         const [y,x]=note;
         if (y<0) return;
@@ -29,7 +16,7 @@ export const stripNotes=(lines,ctx)=>{
         if (pn && x<pn.index+pn[0].length) {
             note[1]='';
         } else {
-            note[1]=pinPos( lines[y], x ,{backward:true,cjk:true});
+            note[1]=pinPos( lines[y], x ,{backward:true,cjk:true,offtext:true});
         }
     })
     ctx.notes=notes.filter(it=>it[0]>=0); //remove the notes which cannot be resolve
