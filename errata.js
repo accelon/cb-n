6a674bbdbd31a4e7d9a547023f98bf67c1b9b562
yap cheah shen <yapcheahshen@gmail.com>
2023-10-27 22:28:29 +0800

    an1 ok

--- errata.js
+++ errata.js
@@ -3,6 +3,6 @@ export default {
     'N66/N66n0034_008.xml': [['cora^senapatī','cora^senapatī']],
     'N19/N19n0007_001.xml' :[
         ['一　集</p>','</p>'],
-        ['<l style="text-indent:0em">歸命彼世尊</l><l style="text-indent:5em">應供等覺者</l>',''],
+        ['<l style="text-indent:0em">歸命彼世尊<caesura style="text-indent:5em;"/>應供等覺者</l>',''],
     ], //不輸出
 };
